fe9028e636765dee1dd924dec849834b93cf3929
1169794338@qq.com <Liziyi521521>
2020-04-29 22:48:53 +0800

    本地存储

--- src/main/java/com/bootx/plugin/localStorage/LocalStorageController.java
+++ src/main/java/com/bootx/plugin/localStorage/LocalStorageController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller - 本地文件存储
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author blackboy
  * @version 1.0
  */
-@Controller("adminLocalStorageController")
+@RestController("adminLocalStorageController")
 @RequestMapping("/api/storage_plugin/local_storage")
 public class LocalStorageController extends BaseController {
 
